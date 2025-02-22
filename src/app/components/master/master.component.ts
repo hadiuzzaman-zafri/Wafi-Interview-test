b4c2304a301033837590a4d691f09530a51d90ed
Zafri <zafri.bd@gmail.com>
2025-02-23 00:22:27 +0530

    secondCommit

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { BookingComponent } from '../components/booking/booking.component';
-import { CalenderComponent } from '../components/calender/calender.component';
-import { WorkflowComponent } from '../components/workflow/workflow.component';
+import { BookingComponent } from '../booking/booking.component';
+import { CalenderComponent } from '../calender/calender.component';
+import { WorkflowComponent } from '../workflow/workflow.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
